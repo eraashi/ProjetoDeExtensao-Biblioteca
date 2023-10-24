d62ddde74bb73c4b0b6adfadad691e8b500e7f82
Icaro Rodrigues <erashi_terakh@hotmail.com>
2023-10-23 23:32:12 -0300

    Initial commit

---
+++ src/Conexao.java
@@ -0,0 +1,37 @@
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+public class Conexao {
+    private static final String URL = "jdbc:mysql://localhost:3306/bancoedi";
+    private static final String USER = "root";
+    private static final String PASS = "";
+    public static Connection getConnection()throws SQLException, ClassNotFoundException {
+        Class.forName("com.mysql.jdbc.Driver");
+        Connection conexao = DriverManager.getConnection(URL, USER, PASS);
+        return conexao;
+    }
+    public static void classConnection(Connection conn) throws Exception{
+        try {
+            Connection conexao = Conexao.getConnection();
+            System.out.println("Conexão obtida com sucesso");
+        } catch (SQLException e) {
+            throw new RuntimeException("Erro ao fechar a conexao com o banco de dados: "+ e);
+        }
+    }
+   /*public static void closeConnection(Connection conn) throws SQLException {
+        closeConnection(conn);
+        Object stmt;
+        if (conn != null) {
+            conn.close();
+        }
+    }
+    public static void closeConnection(Connection com, PreparedStatement stmt, ResultSet rs) throws SQLException {
+        closeConnection(com);
+        if (rs != null) {
+            rs.close();
+        }
+    }*/
+}
+
