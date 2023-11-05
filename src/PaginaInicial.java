f982609112e56bd0e6c47a8aa52f3e7841d3b7bd
Icaro Rodrigues <erashi_terakh@hotmail.com>
2023-11-05 20:36:43 -0300

    Reorganização da interface gráfica
    reorganizei alguns elementos da interface gráfica para melhor experiência de usuário e criei novos botões os quais configurarei mais tarde

--- src/PaginaInicial.java
+++ src/PaginaInicial.java
@@ -64,22 +64,25 @@ public class PaginaInicial extends javax.swing.JFrame {
     private void initComponents() {
 
         listaPesquisaLivros = new javax.swing.JList<>();
+        txtNomeUsuario1 = new javax.swing.JLabel();
+        btnNovoLivro = new javax.swing.JButton();
         btnNovaMovimento = new javax.swing.JButton();
-        jLabel3 = new javax.swing.JLabel();
         btnSair = new javax.swing.JButton();
         txtNomeUsuario = new javax.swing.JLabel();
         btnPesquisar = new javax.swing.JButton();
         txtPesquisa = new javax.swing.JTextField();
-        jLabel2 = new javax.swing.JLabel();
-        jLabel4 = new javax.swing.JLabel();
         jLabel14 = new javax.swing.JLabel();
         jLabel8 = new javax.swing.JLabel();
-        jLabel15 = new javax.swing.JLabel();
-        jScrollPane1 = new javax.swing.JScrollPane();
-        tabelaLivro = new javax.swing.JTable();
         jScrollPane3 = new javax.swing.JScrollPane();
         listaLivros = new javax.swing.JList<>();
+        jLabel2 = new javax.swing.JLabel();
+        jLabel15 = new javax.swing.JLabel();
+        jLabel3 = new javax.swing.JLabel();
+        jLabel4 = new javax.swing.JLabel();
+        jScrollPane1 = new javax.swing.JScrollPane();
+        tabelaLivro = new javax.swing.JTable();
         btnAtualizarTabela = new javax.swing.JButton();
+        btnExcluirLivro = new javax.swing.JButton();
         jLabel9 = new javax.swing.JLabel();
         txtId = new javax.swing.JTextField();
 
@@ -93,22 +96,35 @@ public class PaginaInicial extends javax.swing.JFrame {
         getContentPane().setLayout(null);
 
         listaPesquisaLivros.setBorder(new javax.swing.border.LineBorder(new java.awt.Color(211, 211, 211), 1, true));
-        listaPesquisaLivros.setModel(new javax.swing.AbstractListModel<String>() {
-            String[] strings = { "Item 1", "Item 2", "Item 3", "Item 4", "Item 5" };
-            public int getSize() { return strings.length; }
-            public String getElementAt(int i) { return strings[i]; }
-        });
         listaPesquisaLivros.addMouseListener(new java.awt.event.MouseAdapter() {
             public void mouseClicked(java.awt.event.MouseEvent evt) {
                 listaPesquisaLivrosMouseClicked(evt);
             }
         });
         getContentPane().add(listaPesquisaLivros);
-        listaPesquisaLivros.setBounds(480, 200, 500, 92);
+        listaPesquisaLivros.setBounds(480, 200, 500, 90);
+
+        txtNomeUsuario1.setFont(new java.awt.Font("Yu Gothic UI", 1, 36)); // NOI18N
+        txtNomeUsuario1.setForeground(new java.awt.Color(239, 125, 9));
+        txtNomeUsuario1.setText("Olá,");
+        getContentPane().add(txtNomeUsuario1);
+        txtNomeUsuario1.setBounds(120, 330, 80, 50);
+
+        btnNovoLivro.setFont(new java.awt.Font("Segoe UI", 1, 14)); // NOI18N
+        btnNovoLivro.setForeground(new java.awt.Color(255, 204, 0));
+        btnNovoLivro.setText("Novo Livro");
+        btnNovoLivro.setEnabled(false);
+        btnNovoLivro.addActionListener(new java.awt.event.ActionListener() {
+            public void actionPerformed(java.awt.event.ActionEvent evt) {
+                btnNovoLivroActionPerformed(evt);
+            }
+        });
+        getContentPane().add(btnNovoLivro);
+        btnNovoLivro.setBounds(60, 450, 180, 50);
 
         btnNovaMovimento.setFont(new java.awt.Font("Segoe UI", 1, 14)); // NOI18N
         btnNovaMovimento.setForeground(new java.awt.Color(255, 204, 0));
-        btnNovaMovimento.setText("Nova Movimentação");
+        btnNovaMovimento.setText("Editar Seus Dados");
         btnNovaMovimento.setEnabled(false);
         btnNovaMovimento.addActionListener(new java.awt.event.ActionListener() {
             public void actionPerformed(java.awt.event.ActionEvent evt) {
@@ -116,13 +132,7 @@ public class PaginaInicial extends javax.swing.JFrame {
             }
         });
         getContentPane().add(btnNovaMovimento);
-        btnNovaMovimento.setBounds(60, 450, 180, 50);
-
-        jLabel3.setFont(new java.awt.Font("Yu Gothic UI", 1, 24)); // NOI18N
-        jLabel3.setForeground(new java.awt.Color(255, 153, 51));
-        jLabel3.setText("Atualização de Catálogo");
-        getContentPane().add(jLabel3);
-        jLabel3.setBounds(840, 260, 280, 30);
+        btnNovaMovimento.setBounds(60, 510, 180, 50);
 
         btnSair.setFont(new java.awt.Font("Segoe UI", 1, 14)); // NOI18N
         btnSair.setText("Sair");
@@ -132,15 +142,15 @@ public class PaginaInicial extends javax.swing.JFrame {
             }
         });
         getContentPane().add(btnSair);
-        btnSair.setBounds(60, 520, 180, 50);
+        btnSair.setBounds(60, 570, 180, 50);
 
-        txtNomeUsuario.setFont(new java.awt.Font("Yu Gothic UI", 1, 36)); // NOI18N
+        txtNomeUsuario.setFont(new java.awt.Font("Yu Gothic UI", 1, 24)); // NOI18N
         txtNomeUsuario.setForeground(new java.awt.Color(239, 125, 9));
         txtNomeUsuario.setText("nomedousuario");
         getContentPane().add(txtNomeUsuario);
-        txtNomeUsuario.setBounds(20, 360, 270, 48);
+        txtNomeUsuario.setBounds(20, 380, 260, 50);
 
-        btnPesquisar.setFont(new java.awt.Font("Yu Gothic UI", 1, 12)); // NOI18N
+        btnPesquisar.setFont(new java.awt.Font("Segoe UI", 1, 12)); // NOI18N
         btnPesquisar.setText("Pesquisar");
         btnPesquisar.setToolTipText("");
         btnPesquisar.setBorder(new javax.swing.border.LineBorder(new java.awt.Color(204, 204, 204), 1, true));
@@ -171,27 +181,38 @@ public class PaginaInicial extends javax.swing.JFrame {
         getContentPane().add(txtPesquisa);
         txtPesquisa.setBounds(480, 170, 500, 30);
 
-        jLabel2.setIcon(new javax.swing.ImageIcon(getClass().getResource("/imagens/folder/newpackage/lateral mero.png"))); // NOI18N
-        getContentPane().add(jLabel2);
-        jLabel2.setBounds(0, 0, 300, 1160);
-
-        jLabel4.setIcon(new javax.swing.ImageIcon(getClass().getResource("/imagens/folder/newpackage/icone atualização (1).png"))); // NOI18N
-        getContentPane().add(jLabel4);
-        jLabel4.setBounds(760, 250, 80, 50);
-
         jLabel14.setFont(new java.awt.Font("Yu Gothic UI", 1, 24)); // NOI18N
         jLabel14.setForeground(new java.awt.Color(255, 153, 51));
         jLabel14.setText("Movimentações");
         getContentPane().add(jLabel14);
-        jLabel14.setBounds(450, 260, 180, 40);
+        jLabel14.setBounds(860, 260, 180, 40);
 
         jLabel8.setIcon(new javax.swing.ImageIcon(getClass().getResource("/imagens/folder/newpackage/pngtree-magnifier-and-background-glass-lens-png-image_4944280 1.png"))); // NOI18N
         getContentPane().add(jLabel8);
-        jLabel8.setBounds(390, 250, 48, 60);
+        jLabel8.setBounds(800, 250, 48, 60);
+
+        jScrollPane3.setViewportView(listaLivros);
+
+        getContentPane().add(jScrollPane3);
+        jScrollPane3.setBounds(790, 320, 310, 199);
+
+        jLabel2.setIcon(new javax.swing.ImageIcon(getClass().getResource("/imagens/folder/newpackage/lateral mero.png"))); // NOI18N
+        getContentPane().add(jLabel2);
+        jLabel2.setBounds(0, 0, 300, 1160);
 
         jLabel15.setIcon(new javax.swing.ImageIcon(getClass().getResource("/imagens/folder/newpackage/livro icone laranja 1.png"))); // NOI18N
         getContentPane().add(jLabel15);
         jLabel15.setBounds(990, 60, 90, 90);
+
+        jLabel3.setFont(new java.awt.Font("Yu Gothic UI", 1, 24)); // NOI18N
+        jLabel3.setForeground(new java.awt.Color(255, 153, 51));
+        jLabel3.setText("Atualização de Catálogo");
+        getContentPane().add(jLabel3);
+        jLabel3.setBounds(440, 260, 280, 30);
+
+        jLabel4.setIcon(new javax.swing.ImageIcon(getClass().getResource("/imagens/folder/newpackage/icone atualização (1).png"))); // NOI18N
+        getContentPane().add(jLabel4);
+        jLabel4.setBounds(360, 250, 80, 50);
 
         tabelaLivro.setModel(new javax.swing.table.DefaultTableModel(
             new Object [][] {
@@ -207,17 +228,7 @@ public class PaginaInicial extends javax.swing.JFrame {
         jScrollPane1.setViewportView(tabelaLivro);
 
         getContentPane().add(jScrollPane1);
-        jScrollPane1.setBounds(800, 320, 300, 200);
-
-        listaLivros.setModel(new javax.swing.AbstractListModel<String>() {
-            String[] strings = { "Item 1", "Item 2", "Item 3", "Item 4", "Item 5" };
-            public int getSize() { return strings.length; }
-            public String getElementAt(int i) { return strings[i]; }
-        });
-        jScrollPane3.setViewportView(listaLivros);
-
-        getContentPane().add(jScrollPane3);
-        jScrollPane3.setBounds(380, 320, 320, 199);
+        jScrollPane1.setBounds(400, 320, 300, 200);
 
         btnAtualizarTabela.setText("Atualizar");
         btnAtualizarTabela.addActionListener(new java.awt.event.ActionListener() {
@@ -226,7 +237,11 @@ public class PaginaInicial extends javax.swing.JFrame {
             }
         });
         getContentPane().add(btnAtualizarTabela);
-        btnAtualizarTabela.setBounds(910, 540, 90, 23);
+        btnAtualizarTabela.setBounds(440, 540, 90, 23);
+
+        btnExcluirLivro.setText("Excluir Livro");
+        getContentPane().add(btnExcluirLivro);
+        btnExcluirLivro.setBounds(560, 540, 94, 23);
 
         jLabel9.setFont(new java.awt.Font("Yu Gothic UI", 1, 36)); // NOI18N
         jLabel9.setForeground(new java.awt.Color(255, 153, 51));
@@ -336,6 +351,10 @@ public class PaginaInicial extends javax.swing.JFrame {
         listaPesquisaLivros.setVisible(true);
     }//GEN-LAST:event_txtPesquisaMouseClicked
 
+    private void btnNovoLivroActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_btnNovoLivroActionPerformed
+        // TODO add your handling code here:
+    }//GEN-LAST:event_btnNovoLivroActionPerformed
+
     /**
      * @param args the command line arguments
      */
@@ -374,7 +393,9 @@ public class PaginaInicial extends javax.swing.JFrame {
 
     // Variables declaration - do not modify//GEN-BEGIN:variables
     private javax.swing.JButton btnAtualizarTabela;
+    private javax.swing.JButton btnExcluirLivro;
     private javax.swing.JButton btnNovaMovimento;
+    private javax.swing.JButton btnNovoLivro;
     private javax.swing.JButton btnPesquisar;
     private javax.swing.JButton btnSair;
     private javax.swing.JLabel jLabel14;
@@ -391,6 +412,7 @@ public class PaginaInicial extends javax.swing.JFrame {
     private javax.swing.JTable tabelaLivro;
     private javax.swing.JTextField txtId;
     private javax.swing.JLabel txtNomeUsuario;
+    private javax.swing.JLabel txtNomeUsuario1;
     private javax.swing.JTextField txtPesquisa;
     // End of variables declaration//GEN-END:variables
     
